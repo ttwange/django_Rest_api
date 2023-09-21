635f21198acdd59b7957368936d88529c1f768fe
ttwange <ttwange2@gmail.com>
2023-09-21 14:52:19 +0300

    Working on the model serializer

--- drinks/models.py
+++ drinks/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Drink(models ):
+class Drink(models.Model):
   name = models.CharField(max_length=200)
   description = models.CharField(max_length=200)
 
