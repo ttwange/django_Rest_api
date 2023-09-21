e510d11a67c1d8463a860a1f21f602a01124e54a
ttwange <ttwange2@gmail.com>
2023-09-21 18:21:51 +0300

    Working on the views of the drinks

--- drinks/views.py
+++ drinks/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Drink 
+from .serializers import DrinkSerializer
 
 # Create your views here.
+def drink_list(request):
+  #fet all the drinks 
+  #serialize them
+  #return json
