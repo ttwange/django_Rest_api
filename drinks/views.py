b9cfb7f53764746d16083fc39c222ee9c77943e6
ttwange <ttwange2@gmail.com>
2023-09-22 06:25:36 +0300

    added the GET method for the drink detail serializer

--- drinks/views.py
+++ drinks/views.py
@@ -19,3 +19,15 @@ def drink_list(request):
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+
+@api_view(['GET','POST','DELETE'])
+def drink_detail (request , pk):
+  try:
+    drink = Drink.objects.get(pk=id)
+  except Drink.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == "GET":
+    serializer = DrinkSerializer(drink)
+    return Response(serializer.data)
