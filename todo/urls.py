8dc82ef7879cdd60dd0a189580f40a3242a78ab7
ttwange <ttwange2@gmail.com>
2023-09-23 07:15:44 +0300

    Creating an api call to hello world

--- todo/urls.py
+++ todo/urls.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from todo_api import urls as todo_urls
 from drinks import urls as drink_urls
+from RandomApi import urls as Ran_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('todos/', include(todo_urls)),
     path('drinks/',include(drink_urls)),
-    #path('RandomApi/', include())
+    path('RandomApi/', include())
 ]
